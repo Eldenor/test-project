c7678263f489d9735dbe9d1d523d55f6750786c1
Rafał Kowalski <rafal.kowalski@cantio.pl>
2018-10-06 11:41:40 +0200

    delate app.java file

--- src/main/java/App.java
+++
@@ -1,6 +0,0 @@
-total 0
-drwxr-xr-x 1 rafal 197609 0 paź  6 10:01 ./
-drwxr-xr-x 1 rafal 197609 0 paź  6 09:45 ../
--rw-r--r-- 1 rafal 197609 0 paź  6 09:46 AnotherClass.java
--rw-r--r-- 1 rafal 197609 0 paź  6 10:10 App.java
-lala
